5eb028fe04a66ef0f786c0692f9502c7c00d9c80
Mivisha <90041857+Mivisha@users.noreply.github.com>
2021-11-17 13:29:03 +0200

    2_4den

---
+++ src/components/StudentInfo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        {{student.name}}
+        <img v-bind:src="student.photo">
+    </div>
+</template>
+<script>
+import Vue from 'vue'
+
+    export default {
+        props: {
+            id: '',
+        },
+        data: function(){
+            return{
+                student: {},
+            };
+        },
+        mounted: function(){
+        Vue.axios.get("http://46.101.212.195:3000/students/" +this.id).then((response)=>{
+            console.log(response.data)
+            this.student = response.data;
+            })
+        },
+        computed:{
+            getCount(){
+                return this.$store.getters.getCont
+            }
+        }
+    }
+</script>
